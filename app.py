1b39a421da58966b5d986cba2cb95efa5ab9c040
kkufieta <katharina.kufieta@gmail.com>
2019-12-02 11:51:53 -0500

    Add TodoList class & migrations

--- app.py
+++ app.py
@@ -21,9 +21,16 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description} {self.completed}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref = 'list', lazy=True)
 
 # Ensure that the tables are created for all the models 
 # that we've declared and they haven't yet been created.
